fb91a8879c72458e1e51505b07afe5276fe9870d
Danielle Man <man.danielleh@gmail.com>
2022-01-24 09:00:28 -0800

    creation

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Placed letters? (- for unknown) ", function (placedLetters) {
+  if (placedLetters.length !== 5) {
+    console.log("Input must be 5 characters long.");
+    rl.close();
+  }
+
+  rl.question("Known good? ", function (knownGood) {
+    rl.question("Known bad? ", function (knownBad) {
+      fs.readFile("words.txt", "utf8", (_err, file) => {
+        const words = file.split("\n");
+
+        console.log(
+          words
+            .filter(
+              (word) =>
+                placedLetters
+                  .split("")
+                  .filter((l, i) => l === "-" || word[i] === l).length ===
+                word.length
+            )
+            .filter(
+              (word) =>
+                knownGood.length < 1 ||
+                knownGood.split("").filter((l) => word.includes(l)).length ===
+                  knownGood.length
+            )
+            .filter(
+              (word) =>
+                knownBad.length < 1 ||
+                knownBad.split("").filter((l) => !word.includes(l)).length ===
+                  knownBad.length
+            )
+        );
+        console.log("Thanks for playing!");
+        rl.close();
+      });
+    });
+  });
+});
+
+rl.on("close", function () {
+  process.exit(0);
+});
